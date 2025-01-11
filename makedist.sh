6838508f6d8816e5789dfba1f2f5f5fffe97fb26
Anders Granlund <granlund23@yahoo.se>
2025-01-11 21:32:27 +0100

    Update makedist.sh

--- makedist.sh
+++ makedist.sh
@@ -5,8 +5,8 @@ rm -f doom/*
 cp README doom/readme
 cp COPYING doom/copying
 cp ../SDL-1.2/README.MiNT doom/readme.sdl
-cp src/doom doom/doom.gtp
-m68k-atari-mint-strip doom/doom.gtp
+cp src/doom doom/doom.prg
+m68k-atari-mint-strip doom/doom.prg
 
 rm -f doom.zip
 zip -o doom.zip doom/*
